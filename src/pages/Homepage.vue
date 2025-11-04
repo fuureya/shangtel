df61c589c796c04f07114461ef2c000c143f6933
fuureya <agiljibrin009@gmail.com>
2025-11-04 13:03:32 +0800

    fix: chats

--- src/pages/Homepage.vue
+++ src/pages/Homepage.vue
@@ -6,7 +6,7 @@
 
   <!-- Main Content -->
   <div class="container mx-auto px-4 py-16">
-    <div class="text-center max-w-3xl mx-auto">
+    <div class="text-center px-10 mx-auto ">
       <h1 class="text-4xl font-bold text-gray-900 mb-6">{{ currentWelcomeData.title }}</h1>
       <p class="text-xl text-gray-600 leading-relaxed mb-8">
         {{ currentWelcomeData.content }}
@@ -56,7 +56,7 @@ const welcomeData = [
   {
     lang: "id",
     title: "Selamat Datang di PT. Shangkuriang Telekomunikasi Indonesia",
-    content: "PT. Shangkuriang Telekomunikasi Indonesia adalah perusahaan yang bergerak di bidang IT Solution, Managed Service, dan System Integration, dengan fokus utama pada penyediaan layanan jaringan internet yang berkualitas dan berkelanjutan. Dengan pengalaman di berbagai kota besar dan daerah pedesaan, kami siap menjadi mitra terpercaya Anda.",
+    content: "Selamat Datang di PT. Shangkuriang Telekomunikasi Indonesia PT.Shangkuriang Telekomunikasi Indonesia adalah perusahaan yang bergerak di bidang IT Solution, Managed Service, dan System Integration, dengan fokus utama pada penyediaan layanan jaringan internet yang berkualitas dan berkelanjutan. Didukung oleh tenaga ahli berpengalaman dan jaringan kerja yang luas, kami berkomitmen untuk memberikan layanan profesional, solusi teknologi inovatif, serta dukungan terbaik bagi pelanggan di berbagai sektor. kami berkomitmen untuk menghadirkan konektivitas berkualitas tinggi, layanan profesional, serta solusi teknologi terintegrasi yang mendukung transformasi digital di Indonesia. Bersama kami, wujudkan konektivitas tanpa batas untuk masa depan yang lebih cerdas dan terhubung.",
     tagline: "“We Do IT With Passion” — Melayani Anda 24x7"
   },
   {
